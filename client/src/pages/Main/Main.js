81baf95910a161763abc0df330c3486955b117ce
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-04 13:44:28 -0500

    added functions for Main

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
+import './Main.css';
 
 class Main extends Component {
+
+    state = {}
+
+    componentDidMount() {
+
+    }
+
+    loadArticles = () => {
+
+    }
+
     render() {
         return(
             <div>
