8211bc8e0abdb453fd094a3d6b617ed516db812d
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-07-05 11:38:37 -0500

    configured server file

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nyt-articles');
+
+app.listen(PORT, () => {
+  console.log(`App listening on http://localhost:${PORT}`);
+});
